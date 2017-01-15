5bd83b4e60d0cd79e9b37473e632e012b26a1b61
邹正敏 <zouzhengmin@gmail.com>
2017-01-15 22:24:42 +0800

    implement step 5

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,13 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   namespace :admin do
-    resources :jobs
+    resources :jobs do
+      member do
+        post :publish
+        post :hide
+      end
+    end
   end
 
-  
+
 end
