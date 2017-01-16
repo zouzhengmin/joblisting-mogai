301f631a5c2d4f3c571908c86917519ccdc230b8
邹正敏 <zouzhengmin@gmail.com>
2017-01-16 16:41:00 +0800

    step 6 order by wage

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,6 +4,8 @@ class Job < ApplicationRecord
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0}
 
+  scope :published, -> { where(is_hidden: false) }
+  scope :recent, -> { order('created_at DESC')}
   def publish!
     self.is_hidden = false
     self.save
@@ -13,5 +15,5 @@ class Job < ApplicationRecord
     self.is_hidden = true
     self.save
   end
-  
+
 end
